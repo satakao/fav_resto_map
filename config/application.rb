aa24fa33dc81f75a8cbfb3e96332eeebe415f770
TakaoSatou <142711130+satakao@users.noreply.github.com>
2023-12-20 17:10:38 +0000

    Fix layout, error_japanese

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RestaurantMap
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # 日本語の言語設定
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
