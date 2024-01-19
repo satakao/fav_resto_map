e2a7a58623da367f480eecc28ea1eaca7e36bf50
TakaoSatou <142711130+satakao@users.noreply.github.com>
2024-01-19 13:42:41 +0000

    Fix search function

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,11 +1,11 @@
 class Admin::SearchesController < ApplicationController
   def search
-    @range = params[:range]
     @search = params[:word]
-    if @range == 'User'
-      @users = User.looks(params[:search], params[:word])
-    elsif @range == 'Post'
-      @posts = Post.looks(params[:search], params[:word])
-    end
+    @results = User.looks(params[:word]) + Post.looks(params[:word])
+    # if @range == 'User'
+    #   @users = User.looks(params[:search], params[:word])
+    # elsif @range == 'Post'
+    #   @posts = Post.looks(params[:search], params[:word])
+    # end
   end
 end
